79610be204a7d854a7757db84241628d4c9b1df2
Yangyang Li <yangyang.li@northwestern.edu>
2023-07-28 14:02:50 -0500

    add result of benchmarking

--- codes/example1.py
+++ codes/example1.py
@@ -1,9 +1,27 @@
-from pxblat.server import Server, Client
+from pxblat import Server
+from pxblat import Client
 
-with Server() as server:
-    client = Client()
-    client.start()
-    # do some stuffs that consuming time
-    # and do not need query result,
-    # then get query result
-    ret = client.get()
+client = Client(
+    host="localhost",
+    port=65000,
+    seq_dir="ref/",
+    min_score=20,
+    min_identity=90,
+)
+
+server_option = Server.create_option().build()
+with Server(
+    host="localhost",
+    port=65000,
+    two_bit="ref/reference.2bit",
+    option=server_option
+) as server:
+    work()  # work that consums time
+    server.wait_for_ready()
+    result1 = client.query("ATCG")
+    result2 = client.query("AtcG")
+    result3 = client.query(["ATCG", "ATCG"])
+    result4 = client.query(["cgTA", "fasta.fa"])
+
+    for hsp in result1.hsps:
+        print(f"{hsp}")
