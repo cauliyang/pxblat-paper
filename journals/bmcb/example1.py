9b0d7ed1fa48d9954a92920c1c2d2066976ae7a7
Yangyang Li <yangyang.li@northwestern.edu>
2024-03-04 14:26:21 -0600

    add content

---
+++ journals/bmcb/example1.py
@@ -0,0 +1,24 @@
+from pxblat import Server, Client
+
+host = "localhost"
+port = 65000
+seq_dir = "."
+two_bit = "./hg38.2bit"
+
+client = Client(
+    host=host,
+    port=port,
+    seq_dir=seq_dir,
+    min_score=20,
+    min_identity=90,
+)
+
+with Server(host, port, two_bit, can_stop=True, step_size=5) as server:
+    server.wait_ready()
+    result1 = client.query("ATCG")
+    result2 = client.query("AtcG")
+    result3 = client.query("test_case1.fa")
+    result4 = client.query(["ATCG", "ATCG"])
+    result5 = client.query(["test_case1.fa"])
+    result6 = client.query(["cgTA", "test_case1.fa"])
+    print(result3[0])  # print result
